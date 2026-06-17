60c34020aec3cca775901bf5613f42fe14599e76
Jentechnology <Ventas@jentechnology.co>
2026-06-16 21:36:52 -0400

    Descripción del cambio

--- seed.py
+++ seed.py
@@ -1,27 +1,21 @@
 from app import create_app
-from app.models.product import db, Product
+from app.models.product import Product, db
 
 app = create_app()
 
 with app.app_context():
-    db.drop_all()
-    db.create_all()
 
-    p1 = Product(
-        nombre="Cloro Industrial",
-        descripcion="Desinfectante fuerte para superficies",
-        precio=9.99,
-        stock=50
-    )
+    productos = Product.query.all()
 
-    p2 = Product(
-        nombre="Escoba Pro",
-        descripcion="Uso industrial resistente",
-        precio=14.99,
-        stock=30
-    )
+    if len(productos) >= 3:
+        productos[0].imagen = "cloro.jpg"
+        productos[1].imagen = "desinfetante.jpg"
+        productos[2].imagen = "escoba.jpg"
+        productos[3].imagen = "guantes.jpg"
 
-    db.session.add_all([p1, p2])
-    db.session.commit()
+        db.session.commit()
 
-    print("✔ Productos creados correctamente")
+        print("✅ Imágenes actualizadas correctamente")
+
+    else:
+        print("⚠️ No hay suficientes productos")
